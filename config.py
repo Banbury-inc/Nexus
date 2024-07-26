e2c55163847a2853756e054c555b2a35c7865f08
Michael Mills <mmills6060@gmail.com>
2024-07-26 15:25:45 -0400

    progress

---
+++ config.py
@@ -0,0 +1,15 @@
+class Config:
+    # General
+    DEBUG = True
+    TESTING = False
+    prompt_as_command_line_argument = False
+    max_tokens = 32 # (Default: 128, -1 = infinite generation, -2 = fill context)
+    show_agent_dialogue = False
+    use_task_management_agent = False
+    use_critic_agent = False
+    critic_rating_threshold = 9 # (Default: 9) Between 0-10
+
+
+    run_squad_benchmark = True
+
+
